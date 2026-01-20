6ddc18ed1466cdec528c76b44cc5dc0a5cc09034
SchoolDev4 <hiddenworrior2016@gmail.com>
2026-01-20 12:45:36 +0100

    MenuFix

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class MenuScript : MonoBehaviour
+{
+    private string sceneName;
+    
+
+    private void Start()
+    {
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1); //Byt till level 1
+    }
+
+    public void Credits()
+    {
+        //todo: lägg till scen för Credits och använd SceneManager för att byta till
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
